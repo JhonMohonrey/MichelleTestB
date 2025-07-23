d1d38868c028a980553c00b064607a6680e8a12b
unknown <johnramos659meow@gmail.com>
2025-07-23 12:17:57 +0800

    update jul 23

--- src/app/header/page.jsx
+++ src/app/header/page.jsx
@@ -7,56 +7,28 @@ import logo from '../img/townImg.png'
 export default function page() {
 
     const router = useRouter()
-    let [activeBtn, setActiveBtn] = React.useState([
-        {
-            isActive: false,
-            name: "Sign in"
-        },
-        {
-            isActive: false,
-            name: "Contact"
-        },
-        {
-            isActive: false,
-            name: "Booking"
-        },
-        {
-            isActive: false,
-            name: "Program"
-        },
-        {
-            isActive: false,
-            name: "Service"
-        },
-        {
-            isActive: false,
-            name: "About"
-        },
-    ])
+    // let [activeBtn, setActiveBtn] = React.useState(
+    //     () => {
+    //         return JSON.parse(localStorage.getItem("saveState")) || 
+    //          [{isActive: false, name: "Sign in"},
+    //             {isActive: false, name: "Contact"},
+    //             {isActive: false, name: "Booking"},
+    //             {isActive: false, name: "Program"},
+    //             {isActive: false, name: "Service"},
+    //             {isActive: false, name: "About"},]
+    //     }
+    // )
 
-    function loginFunction() {
-        router.push('/login')
-    }
+    // React.useEffect(() => {
+    //     localStorage.setItem("saveState", JSON.stringify(activeBtn))
+    // }, [activeBtn])
+    let [activeBtn, setActiveBtn] = React.useState( [{isActive: false, name: "Sign in"},
+                {isActive: false, name: "Contact"},
+                {isActive: false, name: "Booking"},
+                {isActive: false, name: "Program"},
+                {isActive: false, name: "Service"},
+                {isActive: false, name: "About"},])
 
-    function ContactFunction() {
-        router.push('/contact')
-    }
-
-    function BookingFunction() {
-        router.push('/booking')
-    }
-
-    function ProgramFunction() {
-        router.push('/program')
-    }
-
-    function ServiceFunction() {
-        router.push('/servicepage')
-    }
-
-    function AboutFunction() {
-        router.push('/about')
-    }
 
     const backHome = () => {
         router.push('/')
@@ -70,9 +42,8 @@ export default function page() {
     const test = (BtnName) => {
         console.log("Testing", BtnName)
         if (BtnName === "About") {
-            AboutFunction()
+            router.push('/about')
             setActiveBtn(prev => {
-                console.log(prev)
                 return prev.map(item => {
                     return {...item, isActive: item.name === BtnName ? true : false}
                 })
@@ -80,9 +51,8 @@ export default function page() {
         }
 
         if (BtnName === "Service") {
-            ServiceFunction()
+            router.push('/servicepage')
             setActiveBtn(prev => {
-                console.log(prev)
                 return prev.map(item => {
                     return {...item, isActive: item.name === BtnName ? true : false}
                 })
@@ -90,9 +60,8 @@ export default function page() {
         }
 
         if (BtnName === "Program") {
-            ProgramFunction()
+            router.push('/program')
             setActiveBtn(prev => {
-                console.log(prev)
                 return prev.map(item => {
                     return {...item, isActive: item.name === BtnName ? true : false}
                 })
@@ -100,9 +69,8 @@ export default function page() {
         }
 
         if (BtnName === "Booking") {
-            BookingFunction()
+            router.push('/booking')
             setActiveBtn(prev => {
-                console.log(prev)
                 return prev.map(item => {
                     return {...item, isActive: item.name === BtnName ? true : false}
                 })
@@ -110,9 +78,8 @@ export default function page() {
         }
 
         if (BtnName === "Contact") {
-            ContactFunction()
+            router.push('/contact')
             setActiveBtn(prev => {
-                console.log(prev)
                 return prev.map(item => {
                     return {...item, isActive: item.name === BtnName ? true : false}
                 })
@@ -120,9 +87,8 @@ export default function page() {
         }
 
         if (BtnName === "Sign in") {
-            loginFunction()
+            router.push('/login')
             setActiveBtn(prev => {
-                console.log(prev)
                 return prev.map(item => {
                     return {...item, isActive: item.name === BtnName ? true : false}
                 })
@@ -130,26 +96,58 @@ export default function page() {
         }
 
     }
+
+    // For Phone User
+    const [menuStatus, setMenuStatus] = React.useState(true)
+    const openMenu = () => {
+        setMenuStatus(prev => !prev)
+    }
     const BtnOptions = () => {
         return activeBtn.map(item => {
             return (
-  <p
-    key={item.name}
-    onClick={() => test(item.name)}
-    className={`
-      relative hover:bg-[#00000030] rounded-[2px]
-      font-bold text-white cursor-pointer active:opacity-50
-      text-[1vw] p-2 select-none
-      transition-all duration-300 ease-in-out
-      after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px]
-      after:w-full after:transition-transform after:duration-200 after:scale-x-0
-      after:origin-left after:bg-white
-      ${item.isActive ? "after:scale-x-100" : ""}
-    `}
-  >
-    {item.name}
-  </p>
-);
+                <div key={item.name}>
+                    <p
+                        onClick={() => test(item.name)}
+                        className={`hidden md:block
+                        relative hover:bg-[#00000030] rounded-[2px]
+                        font-bold text-white cursor-pointer active:opacity-50
+                        md:text-[clamp(0.875rem,1vw,1.25rem)] text-[clamp(1rem, 2vw, 2rem)] p-2 select-none
+                        transition-all duration-300 ease-in-out
+                        after:content-[''] after:absolute after:left-0 after:bottom-0 after:h-[2px]
+                        after:w-full after:transition-transform after:duration-200 after:scale-x-0
+                        after:origin-left after:bg-white
+                        ${item.isActive ? "after:scale-x-100" : ""}
+                        `}
+                    >
+                        {item.name}
+                    </p>
+                    <p
+                        className={`
+                            md:hidden
+                            block
+                            text-sm
+                            font-medium
+                            px-5
+                            py-2.5
+                            mx-2
+                            rounded-2xl
+                            ${item.isActive ? "bg-white text-black " : "bg-black text-white "}
+                            shadow-sm
+                            hover:shadow-lg
+                            active:scale-95
+                            transition-all
+                            duration-200
+                            select-none
+                            my-2
+                            cursor-pointer
+                        `}
+                        onClick={() => test(item.name)}
+                    >
+                        {item.name}
+                    </p>
+                </div>
+                
+            );
 
         })
     }
@@ -171,13 +169,35 @@ export default function page() {
                 {BtnOptions()}
             </nav>
 
-            <button className="md:hidden text-gray-300 hover:text-white">
+            <button onClick={openMenu} className="md:hidden text-gray-300 hover:text-white">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none"
                     viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                 <path stroke-linecap="round" stroke-linejoin="round"
                         d="M4 6h16M4 12h16M4 18h16"/>
                 </svg>
             </button>
+            <div  className={`
+            transition-all duration-400 ease-in-out md:hidden
+            absolute top-0 right-0 bottom-0 w-[50%] bg-[#000000ab]
+            ${menuStatus ? "translate-x-0 opacity-100" : "translate-x-full opacity-0"}
+            `}
+            >
+                <div className='bg-black flex items-end py-3 px-2'>
+                    <button onClick={openMenu} className="text-white hover:text-gray-200">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-6 w-6"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                            strokeWidth="2"
+                        >
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a2 2 0 01-2 2H5a2 2 0 01-2-2V7a2 2 0 012-2h6a2 2 0 012 2v1" />
+                        </svg>
+                    </button>
+                </div>
+                {BtnOptions()}
+            </div>
         </div>
       </header>
     );
--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,24 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 function page(props) {
+    const router = useRouter();
+
     return (
         <div className='flex items-center justify-center border-2 border-black'>
-            <div className='border-2 w-full border-red-500 max-w-7xl'>
+            <div className='border-2 w-full border-red-500 max-w-7xl flex flex-col items-center justify-center'>
                 <p className='font-bold text-2xl text-center'>This is Login Page</p>
+
+                <div className='border-2 border-red-500 flex'>
+                    <div className='border border-blue-600'>
+                        {/* Image Section */}
+                        <p>This is Image Section</p>
+                    </div>
+                    <div className='border border-blue-600'>
+
+                    </div>
+                </div>
 
             </div>
         </div>
